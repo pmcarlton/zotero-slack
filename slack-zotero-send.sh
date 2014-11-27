8728388632913389f261ceafaaa41bb962009e07
Peter Carlton <pmcarlton@mac.com>
2014-11-27 21:15:51 +0900

    Update slack-zotero-send.sh
    make it easier to see how to add your webhook URL

--- slack-zotero-send.sh
+++ slack-zotero-send.sh
@@ -3,7 +3,9 @@
 channel=\"#papers\";
 username=\"zotero_bot\";
 icon_emoji=\":cow:\";
-URL=*REPLACE WITH YOUR WEBHOOK URL*
+URL='PLACE YOUR WEBHOOK URL WITHIN THESE SINGLE QUOTES'
+#for example:
+#URL='https://hooks.slack.com/foo/bar/000slack0'
 while read TEXT; do
 echo curl -s -X POST --data-urlencode \'payload={\"channel\": $channel, \"username\": $username, \"text\": \"$TEXT\", \"icon_emoji\": $icon_emoji}\' $URL | sh
 echo $TEXT >> $LOGDIR/slack-zotero.log
