1187d90ccdedce753da53120cb2703bfa68a0017
Andrew Leifer <leifer@princeton.edu>
2017-12-24 15:11:11 -0500

    WARNING THIS COMMIT HAS OUR PRIVATE SLACK KEY IN NIT

--- slack-zotero-send.sh
+++ slack-zotero-send.sh
@@ -3,13 +3,13 @@
 channel=\"#papers\";
 username=\"zotero_bot\";
 icon_emoji=\":cow:\";
-URL='PLACE YOUR WEBHOOK URL WITHIN THESE SINGLE QUOTES'
+URL='https://hooks.slack.com/services/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'
 #for example:
 #URL='https://hooks.slack.com/foo/bar/000slack0'
 while read TEXT; do
 echo curl -s -X POST --data-urlencode \'payload={\"channel\": $channel, \"username\": $username, \"text\": \"$TEXT\", \"icon_emoji\": $icon_emoji}\' $URL | sh
-echo $TEXT >> $LOGDIR/slack-zotero.log
-date >> $LOGDIR/slack-zotero.log
+echo $TEXT >> slack-zotero.log
+date >> slack-zotero.log
 sleep 2
 done
 exit 0
